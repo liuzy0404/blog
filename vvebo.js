a137222bd1daf3c9f5e02e7791b51df0913dfe60
zhenyuanliu <zhenyuanliu@sohu-inc.com>
2025-12-26 17:43:41 +0800

    add local module script

--- vvebo.js
+++ vvebo.js
@@ -43,3 +43,22 @@ if (url.includes("users/show")) {
 } else {
   $done({});
 }
+
+
+/* surge local module
+#!name=vvebo
+#!desc=修复 vvebo 用户主页显示问题（适配卡片式 API）
+
+[Script]
+fix-vvebo-user-show = type=http-request, pattern=^https://api.weibo.cn/2/users/show?, max-size=1048576, script-path=https://raw.githubusercontent.com/liuzy0404/blog/refs/heads/master/vvebo.js
+fix-vvebo-user-timeline = type=http-request, pattern=^https://api.weibo.cn/2/statuses/user_timeline?, max-size=1048576, script-path=https://raw.githubusercontent.com/liuzy0404/blog/refs/heads/master/vvebo.js
+fix-vvebo-user-unread-count = type=http-request, pattern=^https://api.weibo.cn/2/remind/unread_count?, max-size=1048576, script-path=https://raw.githubusercontent.com/liuzy0404/blog/refs/heads/master/vvebo.js
+fix-vvebo-user-profile = type=http-response, pattern=^https://api.weibo.cn/2/profile/statuses/tab?, max-size=5242880, script-path=https://raw.githubusercontent.com/liuzy0404/blog/refs/heads/master/vvebo.js, requires-body=true
+fix-vvebo-user-cardlist = type=http-response, pattern=^https://api.weibo.cn/2/cardlist?, max-size=5242880, script-path=https://raw.githubusercontent.com/liuzy0404/blog/refs/heads/master/vvebo.js, requires-body=true
+[MITM]
+hostname = api.weibo.cn
+
+[Variable]
+# 可选：用于调试时查看当前 UID
+weibouid = string
+*/
